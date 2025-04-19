5b8c8a02eaeef745363c524fa0e31f6ab4337863
DerHyper <maxrichter15@web.de>
2025-04-19 19:50:58 +0200

    Add 1.2

--- os/src/user/aufgabe1/keyboard_demo.rs
+++ os/src/user/aufgabe1/keyboard_demo.rs
@@ -7,7 +7,12 @@ use crate::devices::keyboard as keyboard; // shortcut for keyboard
 pub fn run() {
 
     /* Hier muss Code einfgeügt werden */
+    let mut cga = cga::CGA.lock();
 
+    loop{
+        let keyboard = keyboard::KEYBOARD.lock();
+        let key = keyboard.key_hit();
+    }
     // 'key_hit' aufrufen und Zeichen ausgeben
 
 }
