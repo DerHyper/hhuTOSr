2432fe4e337fc2b08a4b0f4c03b6d3ffbc7f8223
DerHyper <maxrichter15@web.de>
2025-05-26 14:27:37 +0200

    Add 4.1 coroutine_demo

--- os/src/user/aufgabe4/coroutine_demo.rs
+++ os/src/user/aufgabe4/coroutine_demo.rs
@@ -1,14 +1,25 @@
 use crate::devices::cga;
-use crate::kernel::coroutines::coroutine::Coroutine;
+use crate::kernel::coroutines::coroutine::{self, Coroutine};
 
 fn coroutine_loop(coroutine: &mut Coroutine) {
-
-    /* Hier muss Code eingefuegt werden */
-
+    for i in 0..5 {
+        println!("Coroutine {}: iteration {}", coroutine.get_id(), i);
+        coroutine.switch(); // Switch to the next coroutine
+    }
 }
 
 pub fn run() {
 
-    /* Hier muss Code eingefuegt werden */
+    // Create 3 coroutines
+    let mut coroutine1 = Coroutine::new(coroutine_loop);
+    let mut coroutine2 = Coroutine::new(coroutine_loop);
+    let mut coroutine3 = Coroutine::new(coroutine_loop);
 
+    // Set the next pointers to form a circular linked list
+    coroutine1.set_next(&mut coroutine2);
+    coroutine2.set_next(&mut coroutine3);
+    coroutine3.set_next(&mut coroutine1);
+
+    // Start the first coroutine
+    coroutine1.start();
 }
