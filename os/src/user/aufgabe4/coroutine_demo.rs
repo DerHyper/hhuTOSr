7b10f42b8d255579333b4925dfb6f26a84eb19a3
DerHyper <maxrichter15@web.de>
2025-05-27 13:24:08 +0200

    Fix 4.1 coroutine_demo

--- os/src/user/aufgabe4/coroutine_demo.rs
+++ os/src/user/aufgabe4/coroutine_demo.rs
@@ -6,17 +6,19 @@ use crate::kernel::coroutines::coroutine::{self, Coroutine};
 fn coroutine_loop(coroutine: &mut Coroutine) {
     let mut i = 0;
 
+    // Loop indefinitely, printing the coroutine ID and a counter
+    // position on the screen is determined by the coroutine ID
     loop {
-        println!("Coroutine {}: iteration {}", coroutine.get_id(), i);
+        let print_offset = 5 + coroutine.get_id();
+        cga::CGA.lock().setpos(5,print_offset);
+        print!("Coroutine [{}]: {}", coroutine.get_id(), i);
+
         coroutine.switch(); // Switch to the next coroutine
         i += 1;
     }
-    
-    
 }
 
 pub fn run() {
-
     // Create 3 coroutines
     let mut coroutine1 = Coroutine::new(coroutine_loop);
     let mut coroutine2 = Coroutine::new(coroutine_loop);
@@ -26,6 +28,10 @@ pub fn run() {
     coroutine1.set_next(&mut coroutine2);
     coroutine2.set_next(&mut coroutine3);
     coroutine3.set_next(&mut coroutine1);
+
+    // Initialize the CGA device
+    cga::CGA.lock().clear(); // Clear the screen
+    println!("Coroutine Demo:");
 
     // Start the first coroutine
     coroutine1.start();
