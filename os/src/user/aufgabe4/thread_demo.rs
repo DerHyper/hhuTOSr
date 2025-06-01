2262957c4b80a8a214279e15c93c230988350df1
DerHyper <maxrichter15@web.de>
2025-06-01 21:00:52 +0200

    Fix 4.4 thread_demo syntax errors

--- os/src/user/aufgabe4/thread_demo.rs
+++ os/src/user/aufgabe4/thread_demo.rs
@@ -1,18 +1,13 @@
 use crate::devices::cga;
-use crate::kernel::threads::scheduler::{get_scheduler, Scheduler};
+use crate::kernel::threads::scheduler;
 use crate::kernel::threads::thread::{self, Thread};
 
 fn thread_entry() {
-
-    println!("Thread [{}]", Scheduler::get_active_tid())
-
+    println!("Thread [{}]", scheduler::get_scheduler().get_active_tid())
 }
 
 pub fn run() {
-
-    /* Hier muss Code eingefuegt werden */
-    let scheduler = Scheduler::get_scheduler();
+    let sched = scheduler::get_scheduler();
     let thread1 = Thread::new(thread_entry);
-    scheduler.ready(thread);
-
+    sched.ready(thread1);
 }
