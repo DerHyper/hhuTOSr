d79688f4485770453df64dbb914dcf06f927fd94
DerHyper <maxrichter15@web.de>
2025-07-06 13:24:36 +0000

    Add 6.2 spinlock to cga

--- os/src/devices/cga.rs
+++ os/src/devices/cga.rs
@@ -8,7 +8,7 @@
    ║ Author: Michael Schoetter, Univ. Duesseldorf, 6.2.2024                  ║
    ╚═════════════════════════════════════════════════════════════════════════╝
 */
-use crate::library::mutex::Mutex;
+use crate::library::spinlock::Spinlock as Mutex;
 use core::fmt::Write;
 use crate::kernel::cpu as cpu;
 
