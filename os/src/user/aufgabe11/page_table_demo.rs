4743cede75c3b8430a925915598ab4bf5b277836
DerHyper <maxrichter15@web.de>
2025-12-14 17:47:59 +0100

    Add 11.1 page_fault_handler tests to demo

--- os/src/user/aufgabe11/page_table_demo.rs
+++ os/src/user/aufgabe11/page_table_demo.rs
@@ -4,4 +4,16 @@ use crate::kernel::paging::pages::{init_kernel_tables, write_cr3};
 pub fn run() {
     let table = init_kernel_tables();
     unsafe { write_cr3(table); }
+
+    test_page_fault();
+    
+}
+
+/// Tries to allocate and write at address 0x0 
+/// This should cause a Page Fault that is catched
+/// by the Page Fault Handler
+fn test_page_fault() {
+    unsafe {
+        core::ptr::read_volatile(0 as *const u64);
+    }
 }
