c8e384d49d5e5630cd6d8d6b5300366cb09265e2
DerHyper <maxrichter15@web.de>
2026-02-28 16:37:21 +0100

    Add upgrade_spawn_timer to UpgradeManager

--- apps/pong/src/upgrades/upgrade_manager.rs
+++ apps/pong/src/upgrades/upgrade_manager.rs
@@ -2,29 +2,43 @@ extern crate alloc;
 
 use alloc::boxed::Box;
 use alloc::vec::Vec;
-use crate::{geometrics::Rect, upgrades::{longerbar::LongerBarUpgrade, upgrade::{Upgrade, UpgradeTypeTrait}}};
+use crate::{geometrics::Rect, pong_game, upgrades::{longerbar::LongerBarUpgrade, upgrade::{Upgrade, UpgradeTypeTrait}}, utils::Timer};
 use usrlib::consts::{CGA_COLUMNS, CGA_ROWS};
+
+const UPGRADE_SPAWN_INTERVAL: usize = 10000; // Spawn upgrade every 10 seconds
 
 pub struct UpgradeManager {
     upgrade_types: Vec<Box<dyn UpgradeTypeTrait>>,
     pub instantiated_upgrades: Vec<Upgrade>,
+    upgrade_spawn_timer: Timer
 }
 
 impl UpgradeManager {
     pub fn new() -> UpgradeManager {
-        let mut upgrade_types = Vec::new();
+        let mut upgrade_types: Vec<Box<dyn UpgradeTypeTrait>> = Vec::new();
         upgrade_types.push(Box::new(LongerBarUpgrade::new()));
 
         UpgradeManager {
-            upgrade_types: Vec::new(),
-            instantiated_upgrades: Vec::new()
+            upgrade_types: upgrade_types,
+            instantiated_upgrades: Vec::new(),
+            upgrade_spawn_timer: Timer::new()
         }
     }
 
+    /// Called every frame
+    pub fn update(&mut self) {
+        if self.upgrade_spawn_timer.elapsed() >= UPGRADE_SPAWN_INTERVAL {
+            self.instantiate_random_upgrade();
+            self.upgrade_spawn_timer.reset();
+        }
+    }
+
+    /// Registers a new upgrade type, which can then be instantiated randomly during the game
     pub fn register_upgrade_type(&mut self, upgrade_type: Box<dyn UpgradeTypeTrait>) {
         self.upgrade_types.push(upgrade_type);
     }
 
+    /// Instantiates a random upgrade at a random position on the screen and adds it to the list of instantiated upgrades
     pub fn instantiate_random_upgrade(&mut self) {
         let upgrade_type = self.pick_random_upgrade_type();
         if upgrade_type.is_none() {
@@ -64,5 +78,4 @@ impl UpgradeManager {
 
         Rect::new(x, y, 1.0, 1.0)
     }
-
 }
